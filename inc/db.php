c8fa2ce29fc0b513527980354c6457e7f958c1ae
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-08-11 16:47:54 +0300

    Feat: DB config file

---
+++ inc/db.php
@@ -0,0 +1,19 @@
+<?php
+// inc/db.php
+$host = 'localhost';
+$db = 'kenya_pos';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    exit('DB Connection failed: ' . $e->getMessage());
+}
